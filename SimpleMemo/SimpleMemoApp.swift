568a755a530c913ed2363f2aa7dd2e6fead94359
minjiLee68 <dlalswl2598@gmail.com>
2022-05-29 22:11:58 +0900

    Initial Commit

---
+++ SimpleMemo/SimpleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMemoApp.swift
+//  SimpleMemo
+//
+//  Created by 이민지 on 2022/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
